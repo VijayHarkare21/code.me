6d005744fe2a7e65e48b6f970df250f0f3561d6b
VijayHarkare21 <VijayHarkare21@users.noreply.github.com>
2021-01-26 17:33:22 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -86,18 +86,21 @@ void print_winner(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (candidates[i].votes == 0 || candidates[j].votes == 0)
+            if (i != j)
             {
-                if (candidates[i].votes > candidates[i].votes)
+                if (candidates[i].votes == 0 || candidates[j].votes == 0)
                 {
-                    max_count = candidates[i].votes;
+                    if (candidates[i].votes > candidates[i].votes)
+                    {
+                        max_count = candidates[i].votes;
+                    }
                 }
-            }
-            else if (candidates[i].votes > 0 && candidates[i].votes > 0)
-            {
-                if (candidates[i].votes >= candidates[j].votes)
+                else if (candidates[i].votes > 0 && candidates[i].votes > 0)
                 {
-                    max_count = candidates[i].votes;
+                    if (candidates[i].votes >= candidates[j].votes)
+                    {
+                        max_count = candidates[i].votes;
+                    }
                 }
             }
         }
