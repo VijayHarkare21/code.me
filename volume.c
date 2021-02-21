be7e549de117998f25ff24cee3f8a85dc62e3dc6
VijayHarkare21 <VijayHarkare21@users.noreply.github.com>
2021-02-21 09:15:23 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -37,7 +37,8 @@ int main(int argc, char *argv[])
     uint8_t header[HEADER_SIZE];
     fread(header, HEADER_SIZE, 1, input);
     fwrite(header, HEADER_SIZE, 1, output);
-    // TODO: Read samples from input file and write updated data to output file
+
+    // Read samples from input file and write updated data to output file
     int16_t buffer;
     while (fread(&buffer, sizeof(int16_t), 1, input))
     {
@@ -45,6 +46,7 @@ int main(int argc, char *argv[])
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }
+
     // Close files
     fclose(input);
     fclose(output);
