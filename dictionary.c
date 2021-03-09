ac1d257ee210c5012e3362f7c242f24f2cddeb40
VijayHarkare21 <VijayHarkare21@users.noreply.github.com>
2021-03-09 11:15:22 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -18,7 +18,7 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 1e9 + 9;
+const unsigned int N = 1000000;
 
 // Hash table
 node *table[N];
@@ -29,28 +29,15 @@ unsigned int word_num = 0;
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
+    // For string word in lowercase
     char tmp_word[LENGTH + 1];
-    for (int i = 0; word[i] != '\0'; i++)
+    for (int i = 0; i < strlen(word) + 1; i++)
     {
-        if (isalpha(word[i]))
-        {
-            if (islower(word[i]))
-            {
-                tmp_word[i] = word[i];
-            }
-            else if (isupper(word[i]))
-            {
-                tmp_word[i] = tolower(word[i]);
-            }
-        }
-        else
-        {
-            tmp_word[i] = word[i];
-        }
+        // Convert the uppercase to lowercase, else preserve lowercase
+        tmp_word[i] = tolower(word[i]);
     }
-    tmp_word[strlen(word)] = '\0';
-    unsigned int hash_v = hash(tmp_word);
-    for (node *cursor = table[hash_v]; cursor != NULL; cursor = cursor->next)
+    // Comparing the word with the words in the linked list at hashed index
+    for (node *cursor = table[hash(tmp_word)]; cursor != NULL; cursor = cursor->next)
     {
         if (strcasecmp(tmp_word, cursor->word) == 0)
         {
@@ -61,58 +48,50 @@ bool check(const char *word)
 }
 
 // Hashes word to a number
-// This hash function is a rolling hash function taken from geeksforgeeks.org
-// Original article contributed by 7maestro and improved by SHIVAMSINGH67, amit143katiyar, 29AjayKumar
-// This hash function is a modified version by me
+// This hash function is a rolling hash function
+// This hash function was found on the internet but there was no author mentioned
 unsigned int hash(const char *word)
 {
-    unsigned int hash_value = 0;
-    int p = 1;
-    for (int i = 0; word[i] != '\0'; i++)
+    unsigned int hash = 0;
+    for (int i = 0 ; word[i] != '\0'; i++)
     {
-        if (isalpha(word[i]))
-        {
-            hash_value += (word[i] - 'a' + 1) * p;
-            p *= 31;
-        }
-        else
-        {
-            continue;
-        }
+        hash = 31 * hash + word[i];
     }
-    return (hash_value % N);
+    return hash % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
+    // Open the file
     FILE *file = fopen(dictionary, "r");
-    if(file == NULL)
+    // Check for any discrepancy
+    if (file == NULL)
     {
         return false;
     }
+    // For temporarily storing words
     char tmpword[LENGTH + 1];
     while (fscanf(file, "%s", tmpword) != EOF)
     {
+        // Allocating memory dynamically
         node *n = malloc(sizeof(node));
+        // For NULL condition
         if (n == NULL)
         {
             return false;
         }
+        // Initially setting for avoiding any discrepancy
         n->next = NULL;
+        // Copying the word for further use
         strcpy(n->word, tmpword);
-        if (table[hash(n->word)] == NULL)
-        {
-            table[hash(n->word)] = n;
-        }
-        else if (table[hash(n->word)] != NULL)
-        {
-            n->next = table[hash(n->word)];
-            table[hash(n->word)] = n;
-        }
-        free(n);
+        // Insert word in the respective linked list
+        n->next = table[hash(n->word)];
+        table[hash(n->word)] = n;
+        // To keep word count
         word_num++;
     }
+    // Closing the file
     fclose(file);
     return true;
 }
@@ -121,6 +100,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     return word_num;
+
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
@@ -129,12 +109,13 @@ bool unload(void)
     for (int i = 0; i < N; i++)
     {
         node *cursor = table[i];
-        while(cursor != NULL)
+        // Iterating through th linked lists and freeing them one by one
+        while (cursor != NULL)
         {
             node *temp = cursor;
             cursor = cursor->next;
             free(temp);
         }
     }
-    return false;
+    return true;
 }
