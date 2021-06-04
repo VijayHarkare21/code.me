ef7e8e4ac239c694a0cd4c15312617057e2a16d6
VijayHarkare21 <VijayHarkare21@users.noreply.github.com>
2021-06-04 05:21:23 +0000

    automated commit by check50 [check50=True]

--- log.sql
+++ log.sql
@@ -24,7 +24,7 @@ WHERE year = 2020 AND month = 7 AND day = 28 AND hour = 10;
 
 -- Finding people with corresponding license plates.
 SELECT license_plate, name FROM people
-WHERE license_plate IN ("L93JTIZ", "322W7JE", "0NTHK55");
+WHERE license_plate IN ("5P2BI95", "94KL13X", "6P58WS2", "4328GD8", "G412CB7", "L93JTIZ", "322W7JE", "0NTHK55");
 
 -- Finding people who withdrew money from the banmk on Fifer street before 10:15 am.
 SELECT account_number, transaction_type, amount FROM atm_transactions
@@ -44,10 +44,14 @@ SELECT id, destination_airport_id, hour, minute FROM flights
 WHERE year = 2020 AND month = 7 AND day = 29 AND origin_airport_id = 8
 ORDER BY hour, minute;
 
--- Check whether the thief went through which flight.
+-- Finding through which flight did the thief and accomplice escape.
+-- Checking for Russell - Philip pair
 SELECT flight_id FROM passengers
 WHERE passport_number IN (SELECT passport_number FROM people WHERE name = "Russell");
+-- Checking for Ernest - Berthold pair
+SELECT flight_id FROM passengers
+WHERE passport_number IN (SELECT passport_number FROM people WHERE name = "Ernest");
 
--- Name of city for the above flight id.
+-- Finding destination airport.
 SELECT city FROM airports
-WHERE id IN (SELECT destination_airport_id FROM flights WHERE id = 18);
+WHERE id = 4;
